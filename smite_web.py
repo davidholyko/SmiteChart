2fe8fe4a8223cde9e1e205229f3e4ae559b72693
David Ko <david.holy.ko@gmail.com>
2018-10-20 01:00:09 -0400

    made char test page and updated char page

--- smite_web.py
+++ smite_web.py
@@ -29,8 +29,23 @@ def data():
     return render_template('home.html', all = all)
 
 @app.route('/gods/<string:id>/')
-@app.route("/test/gods/<string:id>")
 def char_page(id):
+    conn = sqlite3.connect("god_attributes.db")
+    c = conn.cursor()
+    attributes = ["Name", "Siege", "Initiation", "Crowd Control", "Wave Clear", "Objective Damage"]
+
+    c.execute("SELECT * FROM god_attributes_table WHERE name = :god", {"god" : id})
+    god_name = c.fetchone()
+
+    c.execute("SELECT * FROM god_icon_table WHERE name = :god", {"god" : id})
+    all = c.fetchone()
+
+    stats = get_stats_from_db(id)
+
+    return render_template('char_page.html', name = god_name, attr = attributes, all = all, stats = stats)
+
+@app.route("/test/gods/<string:id>")
+def char_test_page(id):
     conn = sqlite3.connect("god_attributes.db")
     c = conn.cursor()
     attributes = ["Name", "Siege", "Initiation", "Crowd Control", "Wave Clear", "Objective Damage"]
@@ -55,7 +70,7 @@ def char_page(id):
 
 
 
-    return render_template('char_page.html', name = god_name, attr = attributes, icon = god_icon, card = god_card, all = all, stats = stats, chart = chart)
+    return render_template('char_test_page.html', name = god_name, attr = attributes, icon = god_icon, card = god_card, all = all, stats = stats, chart = chart)
 
 if __name__ == '__main__':
   app.run(debug = True)
