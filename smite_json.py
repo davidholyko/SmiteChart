d6e866d16c5c684575c9c5f95f2a8e5d24b5574a
David Ko <david.holy.ko@gmail.com>
2018-10-19 22:07:51 -0400

    added plot.py

--- smite_json.py
+++ smite_json.py
@@ -29,7 +29,7 @@ def make_icon_json():
         data.append({"name" : item["Name"],
                      "godIcon_URL": item["godIcon_URL"],
                      "godCard_URL": item["godCard_URL"]})
-
+                     
       gods_json = json.dumps(data)
       json.dump(data, f, indent = 1)
 
